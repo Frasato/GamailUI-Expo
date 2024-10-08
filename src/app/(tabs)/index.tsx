d3eae53326fde91579a6c973e4a7803ef2de46f6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-08 16:17:25 -0300

    Import components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,15 @@
+import { Input } from "@/components/input";
+import { MenuButton } from "@/components/menu-button";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Home(){
     return(
-        <View>
-            <Text>Home</Text>
+        <View className="flex-1 bg-gray-900 pt-14 p-4">
+            <Input>
+                <MenuButton />
+                <Input.Field placeholder="Search on e-mail" />
+            </Input>
         </View>
     );
 }
