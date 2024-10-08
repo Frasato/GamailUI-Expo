a047b49e42c81deb35e0e3cc3127bcb101f10f37
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-08 15:26:37 -0300

    Create te entry point

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import "@/styles/global.css";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Slot } from "expo-router";
+
+export default function Layout(){
+    return(
+        <GestureHandlerRootView>
+            <Slot />
+        </GestureHandlerRootView>
+    );
+}
