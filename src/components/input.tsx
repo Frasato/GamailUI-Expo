cb4e934708734dda45b3910145a03788763c5793
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-08 16:17:19 -0300

    Create Input Component

---
+++ src/components/input.tsx
@@ -0,0 +1,25 @@
+import { colors } from "@/styles/colors";
+import { ReactNode } from "react";
+import { TextInput, TextInputProps, View } from "react-native";
+
+type InputProps = { 
+    children: ReactNode
+}
+
+function Input({children}: InputProps){
+    return(
+        <View className="w-full h-14 bg-gray-800 rounded-lg p-4 flex-row items-center gap-4">
+            {children}
+        </View>
+    )
+}
+
+function InputField({...rest}: TextInputProps){
+    return(
+        <TextInput className="flex-1 font-normal text-base text-white" placeholderTextColor={colors.gray[400]} {...rest}/>
+    )
+}
+
+Input.Field = InputField;
+
+export { Input };
