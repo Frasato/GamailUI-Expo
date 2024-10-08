9e5873ea785af1f9483238d785c50d37e060e1c9
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-08 15:53:24 -0300

    Import fonts and loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,30 @@
 import "@/styles/global.css";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@/components/loading";
 
 export default function Layout(){
+    
+    const [fonstLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold
+    });
+
+    if(!fonstLoaded){
+        return <Loading/>
+    }
+    
     return(
-        <GestureHandlerRootView>
+        <GestureHandlerRootView style={{flex: 1}}>
+            <StatusBar style="light"/>
             <Slot />
         </GestureHandlerRootView>
     );
