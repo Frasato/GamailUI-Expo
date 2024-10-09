7906b6ca28d80e946bfe82b0dede8f7c816d8d64
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-09 10:47:25 -0300

    Create drawer component

---
+++ src/components/drawer-content.tsx
@@ -0,0 +1,28 @@
+import { DrawerContentComponentProps } from "@react-navigation/drawer";
+import { View, Image, ScrollView } from "react-native";
+
+export function DrawerContent(drawerProps: DrawerContentComponentProps){
+    return(
+        <View className="flex-1 bg-gray-600 overflow-hidden">
+            <View className="mt-20 w-full border-b pb-6 border-gray-500">
+                <Image source={require('@/assets/logo.png')} className="w-28 ml-5" resizeMode="contain"/>
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 42}}>
+                <View>
+                    {drawerProps.state.routes.map((route, index)=>{
+                        const isFocused = drawerProps.state.index === index;
+                        const option = drawerProps.descriptors[route.key].options
+
+                        if(option.title === undefined){
+                            return;
+                        }
+
+                        return(
+                            <View key={index}/>
+                        )
+                    })}
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
