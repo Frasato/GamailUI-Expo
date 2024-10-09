f909fbeb22e403cb7f116081991278654934cf79
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-09 09:55:27 -0300

    Finish bottom menu

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout(){
@@ -19,9 +20,21 @@ export default function TabLayout(){
             tabBarActiveTintColor: colors.orange[500],
             tabBarInactiveTintColor: colors.gray[400],
         }}>
-            <Tabs.Screen name="index"/>
-            <Tabs.Screen name="chat"/>
-            <Tabs.Screen name="meeting"/>
+            <Tabs.Screen name="index" options={{
+                tabBarIcon: ({size, color}) => (
+                    <MaterialIcons name="email" size={size} color={color} />
+                )
+            }}/>
+            <Tabs.Screen name="chat" options={{
+                tabBarIcon: ({size, color}) => (
+                    <MaterialIcons name="chat-bubble" size={size} color={color} />
+                )
+            }}/>
+            <Tabs.Screen name="meeting" options={{
+                tabBarIcon: ({size, color}) => (
+                    <MaterialIcons name="videocam" size={size} color={color} />
+                )
+            }}/>
         </Tabs>
     )
 }
