098711c844600815e938fd52f0c529800769213a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-09 09:56:58 -0300

    Change padding

--- src/components/float-button.tsx
+++ src/components/float-button.tsx
@@ -4,7 +4,7 @@ import { Pressable, Text} from "react-native";
 
 export function FloatButton(){
     return(
-        <Pressable className="px-8 py-4 bg-gray-600 rounded-full absolute bottom-4 right-4 flex-row gap-1 items-center">
+        <Pressable className="px-5 py-3 bg-gray-600 rounded-full absolute bottom-4 right-4 flex-row gap-1 items-center">
             <MaterialIcons name="edit" size={22} color={colors.orange[500]}/>
             <Text className="font-subtitle text-orange-500">Write</Text>
         </Pressable>
